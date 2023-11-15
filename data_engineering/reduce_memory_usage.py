9371b3c049f927ddb9759f4d7350c2ca88bab988
Stoyan Kostadinov <stoyank127@gmail.com>
2023-11-15 13:50:23 +0100

    add utility functions borrowed from kaggle

---
+++ data_engineering/reduce_memory_usage.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def reduce_memory_usage(df):
+    """
+    Reduce memory usage of a dataframe by downcasting the data types of the columns.
+
+    Borrowed from https://www.kaggle.com/code/oldwine357/removing-highly-correlated-features
+
+    :param df: the dataframe to reduce memory usage
+    :return: df: the dataframe with reduced memory usage
+    """
+    start_memory = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe is {start_memory} MB")
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != 'object':
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    pass
+        else:
+            df[col] = df[col].astype('category')
+
+    end_memory = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe after reduction {end_memory} MB")
+    print(f"Reduced by {100 * (start_memory - end_memory) / start_memory} % ")
+    return df
---
+++ data_engineering/remove_collinear_features.py
@@ -0,0 +1,34 @@
+def remove_collinear_features(df, threshold):
+    """
+    Removes collinear features in a dataframe with a correlation coefficient
+    greater than the threshold. Removing collinear features can help a model
+    to generalize and improves the interpretability of the model.
+
+    Borrowed from https://www.kaggle.com/code/oldwine357/removing-highly-correlated-features
+
+    :param df: features dataframe
+    :param threshold: features with correlations greater than this value are removed
+    :return: df: dataframe that contains only the non-highly-collinear features
+    """
+
+    # Calculate the correlation matrix
+    corr_matrix = df.corr()
+    iters = range(len(corr_matrix.columns) - 1)
+    drop_cols = []
+
+    # Iterate through the correlation matrix and compare correlations.
+    for i in iters:
+        for j in range(i+1):
+            item = corr_matrix.iloc[j:(j+1), (i+1):(i+2)]
+            col = item.columns
+            val = abs(item.values)
+
+            # If correlation exceeds the threshold.
+            if val >= threshold:
+                drop_cols.append(col.values[0])
+
+    # Drop one of each pair of correlated columns.
+    drops = set(drop_cols)
+    df = df.drop(columns=drops)
+    print(f'Removed Columns {drops}')
+    return df
