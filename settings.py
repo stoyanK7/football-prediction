dd0ba91ae6e88cf818eb9e323005051bdf715948
Stoyan Kostadinov <stoyank127@gmail.com>
2023-11-16 16:48:02 +0100

    scrape odds data

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+FDCUK_DIR = os.path.join(ROOT_DIR, 'data', 'football-data.co.uk')
+FDCUK_BUNDESLIGA_DIR = os.path.join(FDCUK_DIR, 'bundesliga')
+
+REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0',
+    'Accept-Language': 'en-US,en;q=0.5',
+}
