c7ddb58d408cbce069ed273c8187e0e2fc26439f
Stoyan Kostadinov <stoyank127@gmail.com>
2023-11-22 18:30:36 +0100

    add test for get_latest_season

--- tests/data/test_fbref_scraper.py
+++ tests/data/test_fbref_scraper.py
@@ -24,3 +24,27 @@ def test_scrape(tmpdir):
     ).read_text()
     assert ',Team,' in output_file
     assert output_file == expected_output_file
+
+
+def test_get_latest_season():
+    """Test get_latest_season()."""
+    scraper = FbrefScraper(
+        html_folder_path=Path(TEST_DATA_DIR, 'test_fbref_scraper'),
+        raw_data_folder_path=Path(),
+        competition='Bundesliga',
+    )
+
+    latest_season = scraper.get_latest_season(
+        [
+            'https_((fbref.com(en(squads(054efa67(2020-2021(matchlogs(all_comps(shooting(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2019-2020(matchlogs(all_comps(keeper(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2019-2020(matchlogs(all_comps(passing(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2017-2018(matchlogs(all_comps(passing_types(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2022-2023(matchlogs(all_comps(gca(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2018-2019(matchlogs(all_comps(defense(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2024-2025(matchlogs(all_comps(possession(Bayern-Munich-Match-Logs-All-Competitions.html',
+            'https_((fbref.com(en(squads(054efa67(2023-2024(matchlogs(all_comps(misc(Bayern-Munich-Match-Logs-All-Competitions.html',
+        ]
+    )
+
+    assert latest_season == '2024-2025'
