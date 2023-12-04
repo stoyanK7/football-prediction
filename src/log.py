11f2f14b277860f39460b41bcb36d9e314cd703a
Stoyan Kostadinov <stoyank127@gmail.com>
2023-12-04 17:38:09 +0100

    delete log file if it exists

--- src/log.py
+++ src/log.py
@@ -15,9 +15,13 @@ def get_logger(file_name: str) -> logging.Logger:
     if not Path(LOGS_DIR).exists():
         Path(LOGS_DIR).mkdir(parents=True)
 
+    log_path = Path(LOGS_DIR, f'{file_name}.log')
+    if log_path.exists():
+        log_path.unlink()
+
     logger = logging.getLogger(file_name)
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(Path(LOGS_DIR, f'{file_name}.log'))
+    handler = logging.FileHandler(log_path)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
