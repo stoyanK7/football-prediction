497ee764330c9aee4d23427fa6d76420acf105ba
Stoyan Kostadinov <stoyank127@gmail.com>
2023-12-07 13:25:16 +0100

    add test for recrawl_errored_pages

--- tests/data/test_fbref_crawler.py
+++ tests/data/test_fbref_crawler.py
@@ -80,3 +80,98 @@ def test_crawl(mocker, tmpdir, requests_mock):
 
     assert mock_sleep.call_count == 10
     assert len(list(crawler.html_folder_path.glob('*'))) == 10
+
+
+def test_recrawl_errored_pages(mocker, tmpdir, requests_mock):
+    crawler = FbrefCrawler(
+        competition_stats_href='/en/comps/20/Bundesliga-Stats',
+        html_folder_path=Path(tmpdir, '.pages'),
+    )
+    mock_sleep = mocker.patch('src.data.fbref_crawler.sleep')
+    requests_mock.get(
+        'https://fbref.com/en/comps/20/Bundesliga-Stats',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'stats_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/Bayer-Leverkusen-Stats',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'leverkusen_filter')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/shooting/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'shooting_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/keeper/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'goalkeeping_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/passing/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'passing_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/passing_types/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'passtypes_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/gca/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(
+                TEST_DATA_DIR,
+                'test_fbref_crawler',
+                'goal_and_shot_creation_table',
+            )
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/defense/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'defensive_actions_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/possession/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'possession_table')
+        ).read(),
+    )
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/misc/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text="""
+        <html>
+            <head>
+                <title>Page Title</title>
+            </head>
+            <body>
+                <h1>403 error</h1>
+            </body>
+        </html>
+        """,
+    )
+
+    crawler.crawl()
+
+    assert mock_sleep.call_count == 10
+    assert len(list(crawler.html_folder_path.glob('**/*.html'))) == 10
+
+    requests_mock.get(
+        'https://fbref.com/en/squads/c7a9f859/2023-2024/matchlogs/all_comps/misc/Bayer-Leverkusen-Match-Logs-All-Competitions',
+        text=open(
+            Path(TEST_DATA_DIR, 'test_fbref_crawler', 'misc_table')
+        ).read(),
+    )
+
+    crawler.recrawl_errored_pages()
+
+    assert mock_sleep.call_count == 11
